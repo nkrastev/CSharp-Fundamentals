b1754ec3fb6f3c3a450bfd988326ca602d1ecae4
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-10-17 16:14:22 +0300

    C# Arrays

---
+++ FundamentalsArrays/Mo05LongestIncreasingSubsequence/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Mo05LongestIncreasingSubsequence
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+
+        }
+    }
+}
