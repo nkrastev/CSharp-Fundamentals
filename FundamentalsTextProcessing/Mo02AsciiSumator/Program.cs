2567a858f45d5ff959888c7d566f36c8f59b897d
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-10-26 17:46:18 +0200

    More ex "Text Processing"

---
+++ FundamentalsTextProcessing/Mo02AsciiSumator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Mo02AsciiSumator
+{
+    class Program
+    {
+        static void Main()
+        {
+            char start = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            int totalSum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i]>start && input[i]<end)
+                {                    
+                    totalSum += (int)input[i];
+                }
+            }
+
+            Console.WriteLine(totalSum);
+        }
+    }
+}
