8d29cd3dc258340848849ed166ff5bc4b4e965b1
nkrastev <nikolay.krystev@gmail.com>
2020-10-27 11:12:49 +0200

    Update, split to methods

--- FundamentalsTextProcessing/Mo02AsciiSumator/Program.cs
+++ FundamentalsTextProcessing/Mo02AsciiSumator/Program.cs
@@ -9,18 +9,25 @@ namespace Mo02AsciiSumator
             char start = char.Parse(Console.ReadLine());
             char end = char.Parse(Console.ReadLine());
             string input = Console.ReadLine();
+         
+            SumAndPrint(input, start, end);
+            
+        }
 
+        static void SumAndPrint(string[] input, char start, char end)
+        {
             int totalSum = 0;
-
             for (int i = 0; i < input.Length; i++)
             {
-                if (input[i]>start && input[i]<end)
-                {                    
+                if (input[i] > start && input[i] < end)
+                {
                     totalSum += (int)input[i];
                 }
             }
 
             Console.WriteLine(totalSum);
         }
+
+
     }
 }
