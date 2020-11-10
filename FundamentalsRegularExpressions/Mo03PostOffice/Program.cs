3b139ad47232af1ca849f0cf252db2294c0544d8
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-11-10 19:06:00 +0200

    Regex Get Capital Letters for task

---
+++ FundamentalsRegularExpressions/Mo03PostOffice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PostOffice
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split("|",StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string capitalLetters = input[0];
+            string secondPart = input[1];
+            string wordsInput = input[2];
+
+            Regex regexCapitalLetters = new Regex(@"([#$%*&])([A-Z]+)\1");
+        }
+    }
+}
