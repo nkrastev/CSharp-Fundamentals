5539c0ee6e2d8d251f902ceef8f5fe7f599da604
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-11-11 17:26:25 +0200

    Post office update

--- FundamentalsRegularExpressions/Mo03PostOffice/Program.cs
+++ FundamentalsRegularExpressions/Mo03PostOffice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -8,12 +9,33 @@ namespace PostOffice
     {
         static void Main()
         {
-            string[] input = Console.ReadLine().Split("|",StringSplitOptions.RemoveEmptyEntries).ToArray();
-            string capitalLetters = input[0];
-            string secondPart = input[1];
-            string wordsInput = input[2];
+            string[] input = Console.ReadLine().Split("|",StringSplitOptions.RemoveEmptyEntries).ToArray();            
 
             Regex regexCapitalLetters = new Regex(@"([#$%*&])([A-Z]+)\1");
+            MatchCollection capLetters = regexCapitalLetters.Matches(input[0]);
+
+            //foreach (Match item in capLetters)
+            //{
+            //    Console.WriteLine(item.Groups[2].Value);
+            //}
+
+            Regex regexWordsLenght = new Regex(@"[0-9]{2}:[0-9]{2}");
+            MatchCollection wordsLenght = regexWordsLenght.Matches(input[1]);
+
+            //foreach (Match item in wordsLenght)
+            //{
+            //    Console.WriteLine(item.Value);
+            //}
+
+            List<string> wordsPart3 = input[2].Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            //foreach (var item in wordsPart3)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            // check if each item in the list wordsPart3 meet the conditions
         }
+
     }
 }
